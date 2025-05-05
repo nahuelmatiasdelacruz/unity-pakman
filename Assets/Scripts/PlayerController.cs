a65715680f37457a10027b7bd6a54bea2c1ee33d
Nahuel De La Cruz <nahuel.delacruz@outlook.com>
2025-05-05 00:07:34 -0300

    Added pacdots logic and player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,51 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    
+
+    public float globalSpeed = 0.1f;
+    private Vector2 destination = Vector2.zero;
+
+    private void Start()
+    {
+        destination = this.transform.position;
+    }
+    private void FixedUpdate()
+    {
+        Vector2 newPosition = Vector2.MoveTowards(this.transform.position, destination, globalSpeed);
+        GetComponent<Rigidbody2D>().MovePosition(newPosition);
+
+        float distanceToDestination = Vector2.Distance((Vector2)this.transform.position, destination);
+
+        if(distanceToDestination < 0.01f)
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && CanMoveTo(Vector2.up))
+            {
+                destination = (Vector2)this.transform.position + Vector2.up;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && CanMoveTo(Vector2.down))
+            {
+                destination = (Vector2)this.transform.position + Vector2.down;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) && CanMoveTo(Vector2.left))
+            {
+                destination = (Vector2)this.transform.position + Vector2.left;
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && CanMoveTo(Vector2.right))
+            {
+                destination = (Vector2)this.transform.position + Vector2.right;
+            }
+        }
+        Vector2 dir = destination - (Vector2)this.transform.position;
+        GetComponent<Animator>().SetFloat("DirX", dir.x);
+        GetComponent<Animator>().SetFloat("DirY", dir.y);
+    }
+
+    bool CanMoveTo(Vector2 direction)
+    {
+        Vector2 start = this.transform.position;
+        Vector2 end = start + direction;
+        RaycastHit2D hit = Physics2D.Linecast(start, end, LayerMask.GetMask("MazeWall"));
+        return hit.collider == null;
+    }
 
 }
